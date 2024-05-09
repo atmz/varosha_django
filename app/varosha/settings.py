b34dcb4c64ca7b8702d96ebae36610eb3bb423f3
Alex Toumazis <alex.toumazis@gmail.com>
2024-05-09 20:42:04 +0300

    CSRF_TRUSTED_ORIGINS

--- app/varosha/settings.py
+++ app/varosha/settings.py
@@ -127,3 +127,5 @@ MEDIA_URL = "https://varosha-media.s3.amazonaws.com/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://6zytaqhsqx.us-east-1.awsapprunner.com/']
