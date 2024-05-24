ae7eaee06466c465c0414548eb3910cdb5bae34e
Alex Toumazis <alex.toumazis@gmail.com>
2024-05-24 10:11:51 +0300

    debug - save locally

--- app/varosha/settings.py
+++ app/varosha/settings.py
@@ -157,4 +157,5 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_REGION_NAME = 'us-east-1' 
 
 # Set default file storage to S3
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
