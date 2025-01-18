525ae929abae73611bfac5da392ff405d7fa5762
Alex Toumazis <alex.toumazis@gmail.com>
2025-01-18 17:13:43 +0200

    tweak textarea

--- app/varosha/forms.py
+++ app/varosha/forms.py
@@ -16,7 +16,7 @@ class NoteForm(ModelForm):
         model = Note
         fields = ['text']
         widgets = {
-            'text': forms.Textarea(attrs={'class': 'note-field', 'rows': 3, 'placeholder': _('Add note, memory or name')}),
+            'text': forms.Textarea(attrs={'class': 'note-field', 'rows': 3, 'placeholder': _('Add note, memory or name'),'cols':30}),
         }
 
 class PointEditForm(ModelForm):
